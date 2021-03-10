a44d39a93b3562c54511f862f2640984fdde7447
karthikaqa <qa@woualabs.com>
2021-03-10 14:24:25 +0530

    Initial Commit

---
+++ MyFirstLibraryDemo/MyFirstLibraryDemo.h
@@ -0,0 +1,18 @@
+//
+//  MyFirstLibraryDemo.h
+//  MyFirstLibraryDemo
+//
+//  Created by WouaLabs QA on 10/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFirstLibraryDemo.
+FOUNDATION_EXPORT double MyFirstLibraryDemoVersionNumber;
+
+//! Project version string for MyFirstLibraryDemo.
+FOUNDATION_EXPORT const unsigned char MyFirstLibraryDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstLibraryDemo/PublicHeader.h>
+
+
---
+++ MyFirstLibraryExample/MyFirstLibraryExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstLibraryExampleApp.swift
+//  MyFirstLibraryExample
+//
+//  Created by WouaLabs QA on 10/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstLibraryExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
